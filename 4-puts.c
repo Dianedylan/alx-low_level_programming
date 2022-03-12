788ab5fc4d6a17deeed7f0934ec654b86bcd08a2
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-12 14:32:51 -0800

    a+x

--- 4-puts.c
+++ 4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
 
 * main - print the string in the put function
@@ -19,8 +18,10 @@ int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
 }
+
+

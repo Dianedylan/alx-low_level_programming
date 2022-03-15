3fc194f14ef75761ca9a876dcc7d8c3be3d67e76
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-15 13:46:06 -0700

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 
 /**
 
-* main - print the string in the put function
+* main - Entry point
 
-*
+*print the string in the put function
 
 * Description: using the main function
 
@@ -19,7 +19,7 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
 
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)	/* _putchar function*/
+
+{
+	char _putchar.c[];
+	puts("%s,\n")
+
+return 0
+}
+

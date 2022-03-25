e53b35ed47e27a03b8acbe2ab2997a07f432910b
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-24 19:57:31 -0700

    678 commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,59 +1,63 @@
-#include "main.h
-/**
- * separators - checks and ensure that all string is capitalized
- * @c: character to be checked
- * Return: if separator return 1. Otherwise return 0;
- */
-int separator(char c)
-{
-switch (c)
-{
-case ' ':
-case '\t':
-case '\n':
-case ',':
-case ';':
-case '.':
-case '!':
-case '?':
-case '"':
-case '(':
-case ')':
-case '{':
-case '}':
-return (1);
+#include "main.h" 
 
-default:
-return (0);
-}
+  
 
+ /** 
 
-}
-/**
- * cap_string - capitalizes all words of a string
- * @s: string to uppercase
- * Return: returns the modified string
- */
-char *cap_string(char *s)
-{
-	int i, j;
-	char spe[13] = {' ', '\t', '\n', ', ', ';' '.', '!', '?', '"', '(', ')', '{', '}' };
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] _= 32;
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] _= 32;
-				}
-			}
-		}
-	}
-	return (s);
+  * cap_string - capitalizes everey word of a string 
 
-}
-		
+  * @s: string to modify 
+
+  * Return: the resulting string 
+
+  */ 
+
+  
+
+ char *cap_string(char *s) 
+
+ { 
+
+         int i, j; 
+
+  
+
+         char spe[13] = {' ', '\t', '\n', ',', ';', '.', 
+
+                 '!', '?', '"', '(', ')', '{', '}'}; 
+
+  
+
+         for (i = 0; s[i] != '\0'; i++) 
+
+         { 
+
+                 if (i == 0 && s[i] >= 'a' && s[i] <= 'z') 
+
+                         s[i] -= 32; 
+
+                 for (j = 0; j < 13; j++) 
+
+                 { 
+
+                         if (s[i] == spe[j]) 
+
+                         { 
+
+                                 if (s[i + 1] >= 'a' && s[i + 1] <= 'z') 
+
+                                 { 
+
+                                         s[i + 1] -= 32; 
+
+                                 } 
+
+                         } 
+
+                 } 
+
+         } 
+
+         return (s); 
+
+ }

398416b49a45117f1e5ecd79a3e9bed50b2b91cb
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-27 02:57:17 -0700

    100 + betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,16 +3,11 @@
 #include <stdlib.h>
 
 /**
-
- * main - main block
-
- * Description: prints all single digit numbers of base 10
-
- * starting from 0, followed by a new line.
-
- * Return: 0
-
- */
+* main - main block
+* Description: prints all single digit numbers of base 10
+* starting from 0, followed by a new line.
+* Return: 0
+*/
 
 int main(void)
 
@@ -29,14 +24,14 @@ while (d < 100)
 
 c = 0;
 
-while (c < 100)
+while (c < 10)
 
 {
 
 
 
-putchar('0' + (d/10));
-putchar('0' + (c%10));
+putchar('0' + (d / 10));
+putchar('0' + (c % 10));
 
 if (c + d != 99)
 
@@ -46,11 +41,11 @@ putchar(',');
 putchar(' ');
 }
 
-c++;
+d++;
 
 }
 
-d++;
+c++;
 
 }
 putchar('\n');

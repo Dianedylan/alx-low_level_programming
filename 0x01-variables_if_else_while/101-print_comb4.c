84154bb5ce8d1259a358d0cf88001c12dbdd3104
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-27 06:31:35 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,105 +4,96 @@
 
 /**
 
- * main - main block
+* main - main block
 
- * Description: Write a program that prints all possible
+* Description: Write a program that prints all possible
 
- * different combinations of two digits
+* different combinations of two digits
 
- * Numbers must be separated by ,, followed by a space
+* Numbers must be separated by ,, followed by a space
 
- * The two digits must be different
+* The two digits must be different
 
- * 01 and 10 are considered the same combination of the two digits 0 and 1
+* 01 and 10 are considered the same combination of the two digits 0 and 1
 
- * Print only the smallest combination of two digits
+* Print only the smallest combination of two digits
 
- * Numbers should be printed in ascending order, with two digits
+* Numbers should be printed in ascending order, with two digits
 
- * You can only use the putchar function
+* You can only use the putchar function
 
- * (every other function (printf, puts, etc…) is forbidden)
+* (every other function (printf, puts, etc…) is forbidden)
 
- * You can only use putchar five times maximum in your code
+* You can only use putchar five times maximum in your code
 
- * You are not allowed to use any variable of type char
+* You are not allowed to use any variable of type char
 
- * All your code should be in the main function
+* All your code should be in the main function
 
- * Return: 0
+* Return: 0
 
- */
+*/
 
 int main(void)
 
 {
 
-	int c;
+int c;
 
-	int d;
+int d;
 
-	int e = 0;
+int e = 0;
 
+while (e < 10)
+{
 
+d = 0;
+while (d < 10)
+{
 
-	while (e < 10)
+c = 0;
 
-	{
+while (c < 10)
+{
+if (c != d && d != e && e < d && d < c)
 
-		d = 0;
+{
 
-		while (d < 10)
+putchar('0' + e);
 
-		{
+putchar('0' + d);
 
-			c = 0;
-
-			while (c < 10)
-
-			{
-
-				if (c != d && d != e && e < d && d < c)
-
-				{
-
-					putchar('0' + e);
-
-					putchar('0' + d);
-
-					putchar('0' + c);
+putchar('0' + c);
 
 
 
-					if (c + d + e != 9 + 8 + 7)
+if (c + d + e != 9 + 8 + 7)
 
-					{
+{
 
-						putchar(',');
+putchar(',');
 
-						putchar(' ');
+putchar(' ');
 
-					}
+}
 
-				}
+}
 
 
 
-				c++;
+c++;
 
-			}
+}
 
-			d++;
+d++;
 
-		}
+}
 
-		e++;
+e++;
 
-	}
-
-	putchar('\n');
-
-	return (0);
+}
+putchar('\n');
+return (0);
 
 }
 

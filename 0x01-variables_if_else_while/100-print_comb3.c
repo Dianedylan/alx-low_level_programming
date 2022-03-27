32d19caaede982ae8a17bc19dff947d00d216acd
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-27 01:58:52 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,51 +17,44 @@
 int main(void)
 
 {
+int c;
 
-	int c;
-
-	int d = 0;
-
-
-
-	while (d < 10)
-
-	{
-
-		c = 0;
-
-		while (c < 10)
-
-		{
+int d = 0;
 
 
 
-			putchar('0' + d);
+while (d < 10)
 
-			putchar('0' + c);
+{
+
+c = 0;
+
+while (c < 10)
+
+{
 
 
 
-			if (c + d != 18)
+putchar('0' + d);
+putchar('0' + c);
 
-			{
+if (c + d != 19)
 
-				putchar(',');
+{
 
-				putchar(' ');
+putchar(',');
+putchar(' ');
+}
 
-			}
+c++;
 
-			c++;
+}
 
-		}
+d++;
 
-		d++;
+}
+putchar('\n');
 
-	}
-
-	putchar('\n');
-
-	return (0);
+return (0);
 
 }

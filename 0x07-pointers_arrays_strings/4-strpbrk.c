be4fa8922b7c13ef7dc62ae740e97760a9ca178e
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-03-28 12:08:54 -0700

    }trickagain

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
 * @s: This is the C string to be scanned.
 * @accept:character in str1 that matches one of the characters in str2
 *Return: string s that matches any character specified in accept
-**/
+*/
 
 char *_strpbrk(char *s, char *accept)
 {
@@ -17,7 +17,7 @@ j = 0;
 while (accept[j] != '\0')  /*Evaluating *accept*/
 {
 if (*s == accept[j])
-}
+{
 return (s);
 }
 
